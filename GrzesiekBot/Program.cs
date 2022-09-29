50bb537c230fe852b126f632e1bc8b0de8c1c12f
Grzesiek <g.pasko@gmail.com>
2022-09-29 17:41:01 +0200

    Init

---
+++ GrzesiekBot/Program.cs
@@ -0,0 +1,77 @@
+﻿using Discord;
+using Discord.Net;
+using Discord.WebSocket;
+using GrzesiekBot.Services;
+using Newtonsoft.Json;
+
+namespace GrzesiekBot;
+
+internal static class Program
+{
+    private static DiscordSocketClient _client = null!;
+    private static JokeService _jokeService = null!;
+    
+    private const long Guild = 0;
+    private const string Token = "";
+
+    public static async Task Main(string[] args)
+    {
+        _client = new DiscordSocketClient();
+        _jokeService = new JokeService(new HttpClient());
+        
+        _client.Log += Log;
+        await _client.LoginAsync(TokenType.Bot, Token);
+        await _client.StartAsync();
+
+        _client.Ready += () =>
+        {
+            Console.WriteLine("Bot is connected!");
+            _client.SetStatusAsync(UserStatus.Online);
+            _client.SetGameAsync("Strażnik Teksasu");
+            return Task.CompletedTask;
+        };
+
+        _client.Ready += BuildCommands;
+        _client.SlashCommandExecuted += SlashCommandHandler;
+
+        await Task.Delay(-1);
+    }
+
+    private static Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+
+    private static async Task BuildCommands()
+    {
+        var guild = _client.GetGuild(Guild);
+        var guildCommand = new SlashCommandBuilder();
+        guildCommand.WithName("give-me-chuck");
+        guildCommand.WithDescription("Let's see what we'll get!");
+
+        var globalCommand = new SlashCommandBuilder();
+        globalCommand.WithName("give-me-chuck");
+        globalCommand.WithDescription("Let's see what we'll get!");
+
+        try
+        {
+            await guild.CreateApplicationCommandAsync(guildCommand.Build());
+            await _client.CreateGlobalApplicationCommandAsync(globalCommand.Build());
+        }
+        catch (HttpException exception)
+        {
+            var json = JsonConvert.SerializeObject(exception.Message, Formatting.Indented);
+            Console.WriteLine(json);
+        }
+    }
+
+    private static async Task SlashCommandHandler(SocketSlashCommand command)
+    {
+        if (command.Data.Name == "give-me-chuck")
+        {
+            var joke = await _jokeService.GetRandomJoke();
+            await command.RespondAsync(joke);
+        }
+    }
+}
---
+++ GrzesiekBot/Services/JokeService.cs
@@ -0,0 +1,28 @@
+﻿using GrzesiekBot.Models;
+using Newtonsoft.Json;
+
+namespace GrzesiekBot.Services;
+
+public interface IJokeService
+{
+    Task<string> GetRandomJoke();
+}
+
+public class JokeService : IJokeService
+{
+    private readonly HttpClient _httpClient;
+    
+    public JokeService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    
+    public async Task<string> GetRandomJoke()
+    {
+        var response = await _httpClient.GetAsync("https://api.chucknorris.io/jokes/random/");
+        response.EnsureSuccessStatusCode();
+        var responseBody = await response.Content.ReadAsStringAsync();
+        var joke = JsonConvert.DeserializeObject<Joke>(responseBody);
+        return joke.value;
+    }
+}
